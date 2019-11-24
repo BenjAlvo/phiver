39cc9553d019a9dfcd4a2de74e0d349fb6050510
Benjamin Alvo <benjamin.alvo@gmail.com>
2019-11-24 15:56:43 +0200

    added controllers

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,19 @@
+class ServicesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/destroy'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :services do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:destroy]
+
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
