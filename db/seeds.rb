a86e3910fec25e640297024573e52fd4281974c4
Susanne Martin <susannemartin@gmx.net>
2019-11-24 19:39:12 +0200

    add database in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name:"test", email:"test@test.test", password:"123456")
-service = Service.create(name:"service", description: "desc", price:"9999999", user:user)
-Booking.create(user: user, service: service)
+# user = User.create(name:"test", email:"test@test.test", password:"123456")
+# service = Service.create(name:"service", description: "desc", price:"9999999", user:user)
+# Booking.create(user: user, service: service)
+
+services = Service.create([
+
+{name: "Babysitting",
+description: "taking care of your children",
+price: 15$},
+
+{name: "Walk your dog",
+description: "walking your dog in the park",
+price: 10$},
+
+{name: "Tutoring Math",
+description: "Tuturoing your childrens Math exercises",
+price: 15$},
+
+{name: "Window cleaning",
+description: "cleaning all your windows",
+price: 20$},
+
+{name: "Webpage development",
+description: "developing your personal webpage",
+price: 30$},
+])
 
