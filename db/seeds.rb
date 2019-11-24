747f2a0ba08f54cb9603af975a0cc95ca7e182df
Ross <ross.a@protonmail.ch>
2019-11-24 23:57:32 +0200

    Addded an interation and susannes data into seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!(name:"test", email:"test@test.test", password:"123456")
-service = Service.create!(name:"service", description: "desc", price:"9999999", user:user)
-Booking.create!(user: user, service: service)
+Service.destroy_all
+puts "Removed the servicess from the list"
+
+hash =  ["services" => {
+{name: "Babysitting",
+description: "Taking care of your children",
+price: 15$},
+{name: "Walk your dog",
+description: "Walking your dog in the park",
+price: 10$},
+{name: "Tutoring Math",
+description: "Tutoring your child's Math exercises",
+price: 15$},
+{name: "Window cleaning",
+description: "cleaning all your windows",
+price: 20$},
+{name: "Webpage development",
+description: "developing your personal webpage",
+price: 30$},
+}]
+
+
+array = hash['services']
+array.each do |minihash|
+  name = minihash[:name]
+  description = minihash[:description]
+  price = minihash[:price]
+  service.create(name:name, description: description, price: price)
+  puts "Added the #{service} service"
+end
+
+# user = User.create!(name:"test", email:"test@test.test", password:"123456")
+# service = Service.create!(name:"service", description: "desc", price:"9999999", user:user)
+# Booking.create!(user: user, service: service)
+
+
+
+
+
+
+
+
 
