cb5d485dcdbeab07ef86079f195fb0bce587c451
Benjamin Alvo <benjamin.alvo@gmail.com>
2019-11-25 17:51:02 +0200

    ghgh

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,8 +4,19 @@ class BookingsController < ApplicationController
   end
 
   def create
+    raise
+    @booking = Booking.new(user_params)
+
   end
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_id)
+  end
+
+
 end
