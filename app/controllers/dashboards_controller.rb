5aa5aa0cd7bd668b68e255c900f76a9c339a5106
Benjamin Alvo <benjamin.alvo@gmail.com>
2019-11-26 11:41:42 +0200

    dashboard done

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,5 @@
+class DashboardsController < ApplicationController
+  def index
+    @bookings = policy_scope(Booking).where(user: current_user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'dashboards/index'
   devise_for :users
   root to: 'services#index'
   resources :services do
     resources :bookings, only: [:show, :new, :create]
   end
   resources :bookings, only: [:destroy]
+  resources :dashboards, only: [:index]
 
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
